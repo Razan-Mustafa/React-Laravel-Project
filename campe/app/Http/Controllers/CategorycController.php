6356b3e671d48e52a24d8d0cb7625ae4bb2c251d
Razan Mustafa <razanmustafa8@gmail.com>
2023-10-17 15:37:41 +0300

    Razan Mustafa

---
+++ campe/app/Http/Controllers/CategorycController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoryc;
+use Illuminate\Http\Request;
+
+class CategorycController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categoryc  $categoryc
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categoryc $categoryc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categoryc  $categoryc
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categoryc $categoryc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Categoryc  $categoryc
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categoryc $categoryc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categoryc  $categoryc
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categoryc $categoryc)
+    {
+        //
+    }
+}
